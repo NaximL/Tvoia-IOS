9178477698daf1b6b2ecc74f30c3d801b4d8a3e4
NaximL <maximloza0@gmail.com>
2026-01-21 12:40:42 +0200

    Initial Commit

---
+++ Tvoia/TvoiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TvoiaApp.swift
+//  Tvoia
+//
+//  Created by Maxim Loza on 21.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct TvoiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
